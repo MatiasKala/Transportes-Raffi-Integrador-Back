2ac6ca46c7f88ec7ec90bb4496826b1cce2bfb35
MatiasKala <matiraffi@gmail.com>
2021-06-11 19:29:52 -0300

    Mejorada la llamada a la API

--- data/hojaDeRuta.js
+++ data/hojaDeRuta.js
@@ -1,22 +1,50 @@
 const axios = require('axios')
 require('dotenv').config()
 
-const openWeatherEndpoint = 
-'http://api.openweathermap.org/data/2.5/weather?'+
-'q=Buenos Aires,ar&APPID='+process.env.OPEN_WEATHER_API_KEY
+class Llamada {    
+    constructor(ciudad) {
+        this.ciudad= ciudad ? ciudad : 'Ciudad Autonoma de Buenos Aires'
+        this.uri='http://api.openweathermap.org/data/2.5/weather?'+
+            'q='+this.ciudad+',ar&APPID='+process.env.OPEN_WEATHER_API_KEY+'&lang=es'
+    }
+}
 
 // Documentacion API
-// https://openweathermap.org/
+// https://openweathermap.org/current
 // Que devuelve? Leer: Weather fields in API response
 
 
-async function getClimaBuenosAires(){
+async function getClimaBuenosAires(ciudadRecibida){
     try {
-        const response = await axios.get(openWeatherEndpoint)
-        return response.data
+
+        var llamada = new Llamada(ciudadRecibida)
+
+        console.log(llamada);
+
+        // if(ciudadRecibida != ''){
+        //      = ciudadRecibida
+        // }
+        
+        const response = await axios.get(llamada.uri)
+        
+        return obtenerDatosNecesarios(response)
+
     } catch (error) {
         throw Error(error)
     }
+}
+
+function obtenerDatosNecesarios(respuesta) {
+    
+    let data = {}
+
+    data.weather = respuesta.data.weather
+    data.main = respuesta.data.main
+    data.clouds = respuesta.data.clouds
+    data.rain = respuesta.data.rain
+    data.name = respuesta.data.name
+
+    return data
 }
 
 module.exports={getClimaBuenosAires}
